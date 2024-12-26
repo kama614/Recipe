c234ca611db48bee52cceba973044ce24db1ae9a
kamada <kamada@example.com>
2024-12-26 11:49:10 +0900

    ListRecipeサーブレットとlistRecipe.jspの記述

--- src/main/java/servlet/ListRecipeServlet.java
+++ src/main/java/servlet/ListRecipeServlet.java
@@ -13,28 +13,35 @@ import dao.DaoFactory;
 import dao.RecipeDao;
 import dto.Recipe;
 
-/**
- * Servlet implementation class RecipeListServlet
- */
-@WebServlet("/resipeList")
-public class RecipeListServlet extends HttpServlet {
+@WebServlet("/listRecipe")
+public class ListRecipeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		/*
+				// セッションの確認
+				HttpSession session = request.getSession(false);
+				if (session == null || session.getAttribute("loggedInAdminId") == null) {
+					response.sendRedirect("login");// ログイン画面にリダイレクト
+					return;
+				}
+		*/
 		try {
 			// レシピデータの取得をDAOに依頼
 			RecipeDao recipeDao = DaoFactory.createRecipeDao(); // RecipeDaoオブジェクトの取得
 			List<Recipe> recipeList = recipeDao.findAll(); // RecipeDaoオブジェクトの利用
 
 			// レシピデータをリクエストスコープに保存
-			request.setAttribute("resipeList", recipeList);
+			request.setAttribute("recipeList", recipeList);
 
-			request.getRequestDispatcher("/WEB-INF/view/recipeList.jsp")
+			// レシピ一覧画面にフォワード
+			request.getRequestDispatcher("/WEB-INF/view/listRecipe.jsp")
 					.forward(request, response);
 
 		} catch (Exception e) {
-			throw new ServletException(e);
+			e.printStackTrace();// デバッグ用。運用環境ではロガーを使用してください。
+			throw new ServletException("レシピ一覧の取得中にエラーが発生しました。", e);
 			// エラー発生した場合にメッセージを表示
 		}
 	}

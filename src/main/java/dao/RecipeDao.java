f8b71efacdcd3b3950f7cabf096b4c5f90446316
kamada <kamada@example.com>
2025-01-14 14:33:36 +0900

    メソッド変更
    findRecommended()　→　findById(Integer id)

--- src/main/java/dao/RecipeDao.java
+++ src/main/java/dao/RecipeDao.java
@@ -5,13 +5,16 @@ import java.util.List;
 import dto.Recipe;
 
 public interface RecipeDao {
-	List<Recipe> findAll() throws Exception;
+	List<Recipe> findAll() throws Exception; //全データを取得
+	 
+	Recipe findById(Integer id) throws Exception; //指定したidの情報を取得
 
-	List<Recipe> findRecommended() throws Exception;
+	void add(Recipe recipe) throws Exception; //追加
 
-	void add(Recipe recipe) throws Exception;
+	void update(Recipe recipe) throws Exception; //更新
 
-	void update(Recipe recipe) throws Exception;
+	void delete(Recipe recipe) throws Exception; // 削除
 
-	void delete(Recipe recipe) throws Exception;
+
+
 }

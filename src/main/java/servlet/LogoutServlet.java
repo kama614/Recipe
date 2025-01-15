06dc7c58abf651fbdf510909b99f8cd9aa48e843
kamada <kamada@example.com>
2025-01-15 14:12:18 +0900

    ログアウトのサーブレット記述完了

--- src/main/java/servlet/LogoutServlet.java
+++ src/main/java/servlet/LogoutServlet.java
@@ -12,15 +12,52 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		performLogout(request, response);
+		/*	
+		  
+		// セッションを無効化
+		request.getSession().invalidate();
+		
+		// ホーム画面にリダイレクト
+		response.sendRedirect(request.getContextPath() + "/home");
+		  
+		response.sendRedirect():
+		ブラウザに「指定されたURLに移動する」よう指示します。
+		クライアント側でURLが変更されるため、リロード時に誤動作が発生しません。
+			
+		request.getContextPath():
+		アプリケーションのルートパスを取得するため、homeのパス指定が環境依存にならず安全です。
+			
+		URLの統一:
+		/WEB-INF/view/home.jspに直接アクセスさせるのではなく、home用のコントローラやサーブレットを作成することを推奨します。
+		*/
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		performLogout(request, response);
+
+		/*		// セッションを無効化
+				request.getSession().invalidate();
+		
+				// ホーム画面にリダイレクト
+				response.sendRedirect(request.getContextPath() + "/home");
+		*/ }
+
+	// doGet,doPostに同じ記述でも問題ないが、メソッドで
+	// まとめることで重複コードを排除してメンテナンス性向上
+	private void performLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		// セッションを無効化
+		request.getSession().invalidate();
+
+		// ホーム画面にリダイレクト
+		response.sendRedirect(request.getContextPath() + "/home");
 	}
 
 }

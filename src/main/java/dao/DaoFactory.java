56697094737ae2c4e332346ae5f37a27a3e08ac3
kamada <kamada@example.com>
2025-01-21 11:59:28 +0900

    DaoFactory編集

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,28 +1,36 @@
 package dao;
 
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class DaoFactory {
 	public static AdminDao createAdminDao() {
 		return new AdminDaoImpl(getDataSource());
 	}
-	
+
 	public static RecipeDao createRecipeDao() {
 		return new RecipeDaoImpl(getDataSource());
 	}
 
 	private static DataSource getDataSource() {
+		InitialContext ctx = null;
 		DataSource ds = null;
 		try {
-			InitialContext ctx = new InitialContext();
+			ctx = new InitialContext();
 			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/recipe_album_db");
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (ctx != null) {
+					ctx.close();
+				}
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+
 		}
 		return ds;
 	}
 }
-
-
-
